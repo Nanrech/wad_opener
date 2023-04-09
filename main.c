2903059e02f0f933ada0ed3d54275ad45a570885
Nan R <88463477+Nanrech@users.noreply.github.com>
2023-04-09 14:16:00 +0200

    Update main.c

--- main.c
+++ main.c
@@ -7,6 +7,20 @@ wad_types
 wad_level
 */
 
+/*
+Hello Hello
+
+If you're reading this, you probably decided to ignore the "warning" in the readme file
+This code is, basically, just an experiment. I wanted to see how DOOM's WAD file format worked so
+I hyper-focused on it for a couple of days and eventually figured out how to "rip" data, or lumps as they're called
+from a wad file. This project isn't finished, and to be frank, I don't know what to do
+with it (level editor? map ripper?), so I'm keeping it like this (raw, unfinished, ugly)
+for the time being in case I ever need a refresher on how to rip lumps from a wad file
+again, or in case I ever want to do the same with Quake's .pak files.
+
+- Nan R. 2023
+*/
+
 int main(int argc, char** argv) {
     // TODO: Replace const with argv member
     const char* WADpath = "doom1.wad";
@@ -26,6 +40,11 @@ int main(int argc, char** argv) {
 
     // Get level from WAD
     level_t level = get_level(&wad_data, wad_file, "E1M1");
+
+    if (level.vertexes == NULL) {
+        puts("ERROR. Couldn't find level.\n");
+        return -1;
+    }
 
     // CLEANUP
     free(level.vertexes);
