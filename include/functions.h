b65e0d558234414dd4ee37cd7ce78ec716d6adad
Nan R <88463477+Nanrech@users.noreply.github.com>
2023-04-20 16:29:50 +0200

    Move functions.h

---
+++ include/functions.h
@@ -0,0 +1,18 @@
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+#include "WAD_types.h"
+
+
+void get_header(WAD_t*, FILE*);
+void get_lumps(WAD_t*, FILE*);
+
+int get_lump_index_by_name(WAD_t*, const char*);
+lump_t get_lump_by_index(WAD_t*, int);
+
+level_t get_level(WAD_t*, FILE*, const char*);
+
+void map_get_things(level_t*, lump_t*, FILE*);
+void map_get_linedefs(level_t*, lump_t*, FILE*);
+void map_get_vertexes(level_t*, lump_t*, FILE*);
